1541df6fdda2f6848e9a924f87255257f794a3fd
youmts <anata0814@gmail.com>
2018-04-09 14:28:31 +0900

    Switch autofac, simple implement

---
+++ LibraryImplement/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using LibraryInterface;
+
+namespace LibraryImplement
+{
+    public class Connection : IConnection
+    {
+        public void Open()
+        {
+            Debug.WriteLine($"Connection#Open called");
+        }
+    }
+}
---
+++ Startup/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using LibraryImplement;
+using LibraryInterface;
+
+namespace Startup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Connection>().As<IConnection>();
+
+            IContainer container = builder.Build();
+
+            var conn = container.Resolve<IConnection>();
+            conn.Open();
+        }
+    }
+}
